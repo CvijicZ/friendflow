b669ae349740c80d2a9339cbfe0be3c3a1477db5
Zoran Cvijic <cvijiczoran4@gmail.com>
2024-07-17 21:45:17 +0200

    Added triggers to sum ids of users on insertation

--- app/migrations/CreateFriendRequestsTable.php
+++ app/migrations/CreateFriendRequestsTable.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Migrations;
 
 class CreateFriendRequestsTable extends Migration
@@ -8,16 +9,26 @@ class CreateFriendRequestsTable extends Migration
         $this->dropIfExists('friend_requests');
 
         $sql = 'CREATE TABLE friend_requests(
-    id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
-    requestor_id int NOT NULL,
-    receiver_id int NOT NULL,
-    sum_of_user_ids INT UNIQUE NOT NULL,
-    status VARCHAR(50) NOT NULL DEFAULT "pending",
-    date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (requestor_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (receiver_id) REFERENCES users(id) ON DELETE CASCADE
-    )';
+                  id int AUTO_INCREMENT PRIMARY KEY NOT NULL,
+                  requestor_id int NOT NULL,
+                  receiver_id int NOT NULL,
+                  sum_of_user_ids INT UNIQUE NOT NULL,
+                  status VARCHAR(50) NOT NULL DEFAULT "pending",
+                  date DATETIME DEFAULT CURRENT_TIMESTAMP,
+                  FOREIGN KEY (requestor_id) REFERENCES users(id) ON DELETE CASCADE,
+                  FOREIGN KEY (receiver_id) REFERENCES users(id) ON DELETE CASCADE
+                 )';
+
         $this->execute($sql, "Database table 'friend_requests' created");
+
+        $triggerSql = 'CREATE TRIGGER before_insert_friend_requests
+                       BEFORE INSERT ON friend_requests
+                       FOR EACH ROW
+                       BEGIN
+                          SET NEW.sum_of_user_ids = NEW.requestor_id + NEW.receiver_id;
+                       END;';
+
+        $this->execute($triggerSql, "Trigger for friend_requests table created");
     }
 
     public function down()
@@ -26,15 +37,3 @@ class CreateFriendRequestsTable extends Migration
         $this->execute($sql, "Database table 'posts' dropped");
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
--- app/migrations/CreateFriendsTable.php
+++ app/migrations/CreateFriendsTable.php
@@ -9,15 +9,25 @@ class CreateFriendsTable extends Migration
         $this->dropIfExists('friends');
 
         $sql = 'CREATE TABLE friends (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    requestor_id INT NOT NULL,
-    receiver_id INT NOT NULL,
-    sum_of_user_ids INT UNIQUE NOT NULL,
-    friendshipType VARCHAR(100),
-    date DATETIME DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (requestor_id) REFERENCES users(id) ON DELETE CASCADE,
-    FOREIGN KEY (receiver_id) REFERENCES users(id) ON DELETE CASCADE)';
+                id INT AUTO_INCREMENT PRIMARY KEY,
+                requestor_id INT NOT NULL,
+                receiver_id INT NOT NULL,
+                sum_of_user_ids INT UNIQUE NOT NULL,
+                friendshipType VARCHAR(100),
+                date DATETIME DEFAULT CURRENT_TIMESTAMP,
+                FOREIGN KEY (requestor_id) REFERENCES users(id) ON DELETE CASCADE,
+                FOREIGN KEY (receiver_id) REFERENCES users(id) ON DELETE CASCADE
+                )';
+
         $this->execute($sql, "Database table 'friends' created");
+
+        $triggerSql = 'CREATE TRIGGER before_insert_friends
+                       BEFORE INSERT ON friends
+                       FOR EACH ROW
+                       BEGIN
+                          SET NEW.sum_of_user_ids = NEW.requestor_id + NEW.receiver_id;
+                       END;';
+        $this->execute($triggerSql, "Trigger for friends table created");
     }
 
     public function down()
